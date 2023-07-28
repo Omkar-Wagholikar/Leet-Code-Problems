6275dbfc96499d6141692b1754e69c86e87903d8
Omkar-Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-28 22:12:02 +0530

    Python solution for permutations

--- BackTracking/Medium/PermutationsI.java
+++ BackTracking/Medium/PermutationsI.java
@@ -43,3 +43,25 @@ public class PermutationsI {
         permute(nums);
     }
 }
+
+//// ==========PYTHON SOLUTION==========
+
+// class Solution(object):
+//     def permute(self, nums):
+//         temp = []
+//         self.fun(temp, nums, [])
+//         return temp
+
+//     def fun(self, finL, a, _stack):
+//         stack = _stack[:]
+//         b = a[:]
+//         if len(stack) == len(a):
+//             finL.append(_stack[:])
+//             stack.pop()
+//             return
+        
+//         for i in range(len(a)):
+//             if b[i] not in stack:
+//                 stack.append(b[i])
+//                 self.fun(finL,a,stack)
+//                 stack.pop()
